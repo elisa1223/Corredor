3c7d984da7718637ef01a8ae71513d9be710e802
elisa1223 <23620091@tlaxiaco.tecnm.mx>
2025-04-02 11:33:15 -0600

    Corredor

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Consolacorredores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Carrera de hilos!");
+
+            // Crear dos corredores
+            Thread corredorA = new Thread(Correr);
+            Thread corredorB = new Thread(Correr);
+
+            corredorA.Start("Corredor A");
+            corredorB.Start("Corredor B");
+
+            corredorA.Join();
+            corredorB.Join();
+
+            Console.WriteLine("¡Carrera terminada!");
+        }
+
+        static void Correr(object nombre)
+        {
+            Random rnd = new Random();
+            for (int pasos = 1; pasos <= 10; pasos++)
+            {
+                Console.WriteLine($"{nombre} avanzó a la posición: {pasos}");
+                Thread.Sleep(rnd.Next(100, 500)); // Velocidad aleatoria
+            }
+            Console.WriteLine($"🏁 {nombre} terminó la carrera!");
+        }
+    }
+
+}
+    
